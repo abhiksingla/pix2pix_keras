86c7acbe904b1b66767de3f830cdbc6a408325b5
abhik <abhiksingla10@gmail.com>
2017-12-24 18:18:41 +0530

    train val loss added after each epoch

--- src/model/train.py
+++ src/model/train.py
@@ -141,11 +141,12 @@ def train(**kwargs):
         disc_loss = None
 
         # generator_model.load_weights("gen_weights21.h5")
+        iter_num = 1
 
         #discriminator_model.load_weights("disc_weights1.2.h5")
 
         #DCGAN_model.load_weights("DCGAN_weights1.2.h5")
-        print "Weights Loaded"
+        print ("Weights Loaded for iter - %d" % iter_num)
 
         # Running average
         losses_list = list()
@@ -160,19 +161,19 @@ def train(**kwargs):
             batch_counter = 1
             start = time.time()
 
-            global disc_n, disc_prev_avg, gen1_n, gen1_prev_avg, gen2_n, gen2_prev_avg, gen3_n, gen3_prev_avg
+            # global disc_n, disc_prev_avg, gen1_n, gen1_prev_avg, gen2_n, gen2_prev_avg, gen3_n, gen3_prev_avg
 
-            disc_n = 1
-            disc_prev_avg = 0
+            # disc_n = 1
+            # disc_prev_avg = 0
 
-            gen1_n = 1
-            gen1_prev_avg = 0
+            # gen1_n = 1
+            # gen1_prev_avg = 0
 
-            gen2_n = 1
-            gen2_prev_avg = 0
+            # gen2_n = 1
+            # gen2_prev_avg = 0
 
-            gen3_n = 1
-            gen3_prev_avg = 0
+            # gen3_n = 1
+            # gen3_prev_avg = 0
 
             for X_full_batch, X_sketch_batch in data_utils.gen_batch(X_full_train, X_sketch_train, batch_size):
 
@@ -206,7 +207,7 @@ def train(**kwargs):
                 # new_avg = ((loss_list_n-1)*prev_avg + disc_loss)/loss_list_n
                 # prev_avg = new_avg
 
-                disc_avg, gen1_avg, gen2_avg, gen3_avg = running_avg(disc_loss, gen_loss[0], gen_loss[1], gen_loss[2])
+                # disc_avg, gen1_avg, gen2_avg, gen3_avg = running_avg(disc_loss, gen_loss[0], gen_loss[1], gen_loss[2])
 
                 # print("running disc loss", new_avg)
                 # print(disc_loss, gen_loss)
@@ -220,8 +221,8 @@ def train(**kwargs):
                                                 ("G logloss", gen_loss[2])])
 
                 # Saving data for plotting
-                losses = [e+1, batch_counter, disc_loss, gen_loss[0], gen_loss[1], gen_loss[2], disc_avg, gen1_avg, gen2_avg, gen3_avg]
-                losses_list.append(losses)
+                # losses = [e+1, batch_counter, disc_loss, gen_loss[0], gen_loss[1], gen_loss[2], disc_avg, gen1_avg, gen2_avg, gen3_avg, iter_num]
+                # losses_list.append(losses)
 
                 # Save images for visualization
                 if batch_counter % (n_batch_per_epoch / 2) == 0:
@@ -238,20 +239,34 @@ def train(**kwargs):
             print("")
             print('Epoch %s/%s, Time: %s' % (e + 1, nb_epoch, time.time() - start))
 
+            #Running average
+            disc_avg, gen1_avg, gen2_avg, gen3_avg = running_avg(disc_loss, gen_loss[0], gen_loss[1], gen_loss[2])
+
+            #Validation loss
+            y_gen_val = np.zeros((X_sketch_batch.shape[0], 2), dtype=np.uint8)
+            y_gen_val[:, 1] = 1
+            val_loss = DCGAN_model.test_on_batch(X_full_batch, [X_sketch_batch, y_gen_val])
+            # print "val_loss ===" + str(val_loss)
+
+            #logging
+            # Saving data for plotting
+            losses = [e+1, iter_num, disc_loss, gen_loss[0], gen_loss[1], gen_loss[2], disc_avg, gen1_avg, gen2_avg, gen3_avg, val_loss[0], val_loss[1], val_loss[2]]
+            losses_list.append(losses)
+
             if (e+1) % 5 == 0:
-                gen_weights_path = os.path.join('../../models/%s/gen_weights_epoch%s.h5' % (model_name, e+1))
+                gen_weights_path = os.path.join('../../models/%s/gen_weights_iter%s_epoch%s.h5' % (model_name, iter_num, e+1))
                 generator_model.save_weights(gen_weights_path, overwrite=True)
 
-                disc_weights_path = os.path.join('../../models/%s/disc_weights_epoch%s.h5' % (model_name, e+1))
+                disc_weights_path = os.path.join('../../models/%s/disc_weights_iter%s_epoch%s.h5' % (model_name, iter_num, e+1))
                 discriminator_model.save_weights(disc_weights_path, overwrite=True)
 
-                DCGAN_weights_path = os.path.join('../../models/%s/DCGAN_weights_epoch%s.h5' % (model_name, e+1))
+                DCGAN_weights_path = os.path.join('../../models/%s/DCGAN_weights_iter%s_epoch%s.h5' % (model_name, iter_num, e+1))
                 DCGAN_model.save_weights(DCGAN_weights_path, overwrite=True)
 
                 loss_array = np.asarray(losses_list)
                 print (loss_array.shape) # 10 element vector
 
-                loss_path = os.path.join('../../losses/loss_epoch%s.csv' % (e+1))
+                loss_path = os.path.join('../../losses/loss_iter%s_epoch%s.csv' % (iter_num, e+1))
                 np.savetxt(loss_path, loss_array, fmt='%.5f', delimiter=',')
                 np.savetxt('test.csv', loss_array, fmt='%.5f', delimiter=',')
 
